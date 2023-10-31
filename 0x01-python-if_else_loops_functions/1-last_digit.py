0b4ba70fe4387774c96a5914a4dd62924112ea7e
TKITO-D-Ahmed <ahmedtkitoemc2@gmail.com>
2023-10-31 11:56:43 -0700

    the second task to get use to if statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ last_digit = abs(number) % 10
 if number < 0:
     last_digit *= -1
 if last_digit > 5:
-    str = f"Last digit of {number} is {last_digit} and is greater that 5"
+    str = f"Last digit of {number} is {last_digit} and is greater than 5"
 elif last_digit == 0:
     str = f"Last digit of {number} is {last_digit} and is 0"
 else:
