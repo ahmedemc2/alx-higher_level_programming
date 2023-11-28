bc2745d24fd257d70ace99c2296196cfc72bfd13
ahmedemc2 <ahmedtkitoemc2@gmail.com>
2023-11-28 22:36:47 +0100

    program that prints all possible different combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for decimal in range(0, 8):
-    for units in range (decimal, 10):
+    for units in range(decimal, 10):
         if decimal != units:
             print("{:d}{:d}".format(decimal, units), end=", ")
 print("{:d}{:d}".format(decimal + 1, units))
