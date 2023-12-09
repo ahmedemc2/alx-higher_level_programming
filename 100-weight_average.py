068bf6ae513a29ce992788045109679cfca796d0
ahmedemc2 <50429714+ahmedemc2@users.noreply.github.com>
2023-12-09 12:59:57 +0100

    Delete 100-weight_average.py

--- 100-weight_average.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-    result = sum(a * b for (a, b) in my_list) / sum(b for (a, b) in my_list)
-    return result
