64e4011e17b6c14da007d804125fa076797fe361
ahmedemc2 <50429714+ahmedemc2@users.noreply.github.com>
2023-12-09 13:02:10 +0100

    Delete 6-print_sorted_dictionary.py

--- 6-print_sorted_dictionary.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    my_list = list(a_dictionary.keys())
-    my_list.sort()
-    for i in my_list:
-        print("{}: {}".format(i, a_dictionary[i]))
