55f912c1dfe68339208725cbacc5cabfcf2152dc
ahmedemc2 <50429714+ahmedemc2@users.noreply.github.com>
2023-12-09 13:02:40 +0100

    Delete 9-multiply_by_2.py

--- 9-multiply_by_2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    new_dict = a_dictionary.copy()
-    for valeur in a_dictionary:
-        cle = a_dictionary[valeur] * 2
-        new_dict.update({valeur: cle})
-    return new_dict
