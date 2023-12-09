6cc0bcc55cbd45b7a4a05a24750d61aeea30a79e
ahmedemc2 <ahmedtkitoemc2@gmail.com>
2023-12-09 12:51:54 +0100

    ya rabii

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 
-matrix = [
+maatrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
 
-new_matrix = square_matrix_simple(matrix)
+new_matrix = square_matrix_simple(maatrix)
 print(new_matrix)
-print(matrix)
+print(maatrix)
